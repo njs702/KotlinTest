f17b53a30f095c163304d7f1e91120abb2a01e71
junseok <njs702@ajou.ac.kr>
2022-01-04 14:39:17 +0900

    JSON, Retrofit implementation added

--- app/src/main/java/com/example/kotlintest/DataAdapter.kt
+++ app/src/main/java/com/example/kotlintest/DataAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 
 class DataAdapter(val dataList: ArrayList<Datas>): RecyclerView.Adapter<DataAdapter.DataViewHolder>(){
--- app/src/main/java/com/example/kotlintest/Datas.kt
+++ app/src/main/java/com/example/kotlintest/Datas.kt
@@ -1,3 +1,4 @@
 package com.example.kotlintest
 
+// Data 담을 Value Object
 class Datas(val text1: String,val text2: String)
